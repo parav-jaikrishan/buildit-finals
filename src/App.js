77f8c2a2d753792d6bff3bad88faf48d50987f01
Parav Jaikrishan <r21947parav@dsprkp.net>
2022-08-24 23:30:06 +0530

    Linked to Shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Shop } from './pages/Shop';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
+        <Route path="/shop" element={<Shop/>}/>
       </Routes>
     </div>
   );
