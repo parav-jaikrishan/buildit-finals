cbf6c406a4aa9db2d0c45549f92f010936f9c654
Parav Jaikrishan <r21947parav@dsprkp.net>
2022-08-25 01:43:56 +0530

    Update

--- src/components/shop-content/ShopContent.js
+++ src/components/shop-content/ShopContent.js
@@ -1,23 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card } from "../card/Card";
+import { ContentModal } from "../content-modal/ContentModal";
 import "./ShopContent.scss";
 
 export const ShopContent = () => {
-    const cards = [{
-
-    }]
+    const smoothieCards = [
+        {
+            name: 'Smoothie',
+            img: "assets/smoothie.png"
+        },
+        {
+            name: 'Smoothie',
+            img: "assets/smoothie1.png"
+        },
+        {
+            name: 'Smoothie',
+            img: "assets/smoothie2.png"
+        }
+    ];
+    const smoothieModals = [
+        {
+            name: 'Smoothie',
+            img: "assets/smoothie.png",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consect",
+            cost: 50
+        },
+        {
+            name: 'Smoothie',
+            img: "assets/smoothie1.png",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consect",
+            cost: 50
+        },
+        {
+            name: 'Smoothie',
+            img: "assets/smoothie2.png",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consect",
+            cost: 50
+        },
+    ];
+    const soupCards = [
+        {
+            name: 'Food 1',
+            img: "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI="
+        },
+        {
+            name: 'Food 1',
+            img: "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI="
+        },
+        {
+            name: 'Food 1',
+            img: "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI="
+        }
+    ];
+    const soupModals = [
+        {
+            name: 'Food 1',
+            img: "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI="
+        },
+        {
+            name: 'Food 1',
+            img: "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI="
+        },
+        {
+            name: 'Food 1',
+            img: "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI="
+        }
+    ];
+    const [openedSmoothieModal, setOpenedSmoothieModal] = useState(
+        Array(smoothieModals.length).fill(false)
+    );
+    const [showModal, setShowModal] = useState("");
+    const openModal = i => {
+        let arr = Array(openedSmoothieModal.length).fill(false);
+        arr[i] = true;
+        setOpenedSmoothieModal([...arr]);
+        setShowModal("show");
+    }
+    const closeModal = () => {
+        setOpenedSmoothieModal(Array(openedSmoothieModal.length).fill(false));
+    }
     return (
         <>
             <div className="shop-content">
                 <div className="shop-section">
-                    <h1>Super Food</h1>
-                    {
-                        cards.map((card, index) => {
-                            return (
-                                <Card key={index} name={card.name} image={card.img} clicked={() => openModal(index)}/>
-                            );
-                        })
-                    }
+                    <h1>Super Smoothies</h1>
+                    <div className="card-container">
+                        {
+                            smoothieCards.map((card, index) => {
+                                return (
+                                    <Card key={index} name={card.name} image={card.img} clicked={() => openModal(index)}/>
+                                );
+                            })
+                        }
+                    </div>
                 </div>
+                <div className="shop-section">
+                    <h1>Super Soups</h1>
+                    <div className="card-container">
+                        {
+                            smoothieCards.map((card, index) => {
+                                return (
+                                    <Card key={index} name={card.name} image={card.img} clicked={() => openModal(index)}/>
+                                );
+                            })
+                        }
+                    </div>
+                </div>
+                {
+                    //eslint-disable-next-line
+                    smoothieModals.map((modal, index) => {
+                    if(openedSmoothieModal[index])
+                        return (
+                            <ContentModal key={index} show={showModal} data={modal} onClose={() => closeModal()} buy={true}/>
+                        );
+                })}
             </div>
         </>
     );
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Navbar } from '../components/navbar/Navbar';
+import { ShopContent } from '../components/shop-content/ShopContent';
 
 export const Shop = () => {
     return (
         <>
             <Navbar/>
+            <ShopContent/>
         </>
     );
 }
