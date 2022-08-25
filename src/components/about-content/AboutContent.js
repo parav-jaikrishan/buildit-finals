85939f018bc0623aa0ec51d54e7629c4e92b2402
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-08-25 10:34:11 +0530

    Update

--- src/components/about-content/AboutContent.js
+++ src/components/about-content/AboutContent.js
@@ -5,7 +5,7 @@ export const AboutContent = () => {
     return (
         <div className="about-content">
             <h1>About Us</h1>
-            <iframe src="https://www.youtube.com/embed/RVdME0VhnjE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe src="https://www.youtube.com/embed/YOxDtGH92Jw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     );
 }
